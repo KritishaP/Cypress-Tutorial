6f0c9c4c425db0f75337674c57cd00e831958605
Kritisha Puri <puriikritisha55@gmail.com>
2026-02-21 16:42:03 +0545

    Sign up and login automated

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('login test suite',()=>{
     it('Login Test',()=>{
         cy.visit('https://automationexercise.com/login')
-        cy.get('[data-qa="login-email"]').type('example@gmail.com')
+        cy.get('[data-qa="login-email"]').type('example98765@gmail.com')
         cy.get('[data-qa="login-password"]').type('12345')
         cy.get('[data-qa="login-button"]').click()
     })
--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -4,5 +4,18 @@ describe('Sign up test suite',()=>{
         cy.get('[data-qa="signup-name"]').type('kritisha')
         cy.get('[data-qa="signup-email"]').type('example98765@gmail.com')
         cy.get('[data-qa="signup-button"]').click()
+
+        cy.get('#id_gender2').check()
+        cy.get('[data-qa="password"]').type('12345')
+        cy.get('[data-qa="first_name"]').type('kritisha')
+        cy.get('[data-qa="last_name"]').type('puri')
+        cy.get('[data-qa="address"]').type('Narendra modi')
+        cy.get('[data-qa="country"]').select('India')
+        cy.get('[data-qa="state"]').type('Gujrat')
+        cy.get('[data-qa="city"]').type('Rajkot')
+        cy.get('[data-qa="zipcode"]').type('00123')
+        cy.get('[data-qa="mobile_number"]').type('9876534278')
+        cy.get('[data-qa="create-account"]').click()
+
     })
 })
